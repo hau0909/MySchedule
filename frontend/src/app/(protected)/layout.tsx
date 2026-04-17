54b4c97ff6bf598744f5a13731c1988e5ea27a69
Hau Nguyen <hau.nguyen.personal@gmail.com>
2026-04-17 13:03:34 +0700

    feat(auth): enhance loading states and redirect logic in ProtectedLayout and Page components

--- frontend/src/app/(protected)/layout.tsx
+++ frontend/src/app/(protected)/layout.tsx
@@ -1,13 +1,38 @@
+"use client";
 // app/(protected)/layout.tsx
 import SideBar from "@/components/layout/sidebar/SideBar";
 import Aside from "@/components/layout/aside/Aside";
 import Header from "@/components/layout/header/Header";
+import { useEffect } from "react";
+import { useAuth } from "@/features/auth";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
 
 export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !user) return router.push("/auth");
+  }, [loading, router, user]);
+
+  if (loading)
+    return (
+      <div
+        className="flex h-screen items-center justify-center gap-2
+        text-xl text-slate-500"
+      >
+        <Loader2 className="animate-spin" />
+        <p className="animate-pulse">Loading</p>
+      </div>
+    );
+
+  if (!user) return null;
+
   return (
     <main className="grid grid-cols-[100px_1fr_332px] h-screen">
       {/* Sidebar */}
